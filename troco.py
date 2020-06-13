bf147eebc0d02ba8960dae65854362bb2835ae96
ArthurL <arthurlibreloncarvalho@gmail.com>
2020-06-12 22:15:35 -0300

    Notas de 100 adicionadas

--- troco.py
+++ troco.py
@@ -1,8 +1,13 @@
 traço = '-'*20
 print(traço)
-print('Calculadora de troco')
+print('Calculadora de troco\n')
 print(traço)
-troco = float(input('Insira o troco')
+#desabilitado para maior velocidade de teste
+troco = float(input('Insira o troco '))
+#troco = 200
 n100 = 0
-int(troco % 100) = n100
-troco = troco - n100*100
+n100 = int(troco / 100)
+troco = troco - (n100*100)
+print('O troco é: \n {} notas de R$100'.format(n100))
+#para teste 
+#print(troco)
