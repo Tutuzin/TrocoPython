d1c4e2bfbba080b7c814647ea1a3dd696f3a88ae
ArthurL <arthurlibreloncarvalho@gmail.com>
2020-06-13 15:17:27 -0300

    Troco com calculo completo e troco2(teste) apagado

--- troco.py
+++ troco.py
@@ -2,8 +2,18 @@ traço = '-'*20
 print(traço)
 print('Calculadora de troco\n')
 print(traço)
-troco = float(input('Insira o troco '))
 #somente para testes
 #troco = 188.9
-import troco2
-
+def calculotroco(troco):
+    psv = (100,50,20,10,5,2,1,0.5,0.25,0.1,0.05)
+    print('O troco é')
+    for vlr in psv:
+        qntd = int(troco / vlr)
+        troco = troco - qntd*vlr
+        print(' {} notas de R${}'.format(qntd,vlr))
+#        print('{:.2f}'.format(troco))
+rpt = 'y'
+while rpt == 'y':
+    troco = float(input('Insira o troco '))
+    calculotroco(troco)
+    rpt = input('Deseja calcular novamente?(y/n)')
--- troco2.py
+++
@@ -1,8 +0,0 @@
-troco = 188.9
-psv = (100,50,20,10,5,2,1,0.5,0.25,0.1,0.05)
-print('O troco é')
-for vlr in psv:
-    qntd = int(troco / vlr)
-    troco = troco - qntd*vlr
-    print(' {} notas de R${}'.format(qntd,vlr))
-print('{:.2f}'.format(troco))
