f1f692806527d02c654169302d127be14ef46797
ArthurL <arthurlibreloncarvalho@gmail.com>
2020-06-13 10:40:26 -0300

    troco agora importa de troco2

--- troco.py
+++ troco.py
@@ -2,15 +2,8 @@ traço = '-'*20
 print(traço)
 print('Calculadora de troco\n')
 print(traço)
-#desabilitado para maior velocidade de teste
-#troco = float(input('Insira o troco '))
-troco = 250
-n100 = int(troco / 100)
-troco = troco - (n100*100)
-print('O troco é: \n {} notas de R$100'.format(n100))
-#para teste 
-n50 = int(troco/50)
-troco = troco - (n50*50)
-print('O troco é: \n {} notas de R$50'.format(n50))
+troco = float(input('Insira o troco '))
+#somente para testes
+#troco = 188.9
+import troco2
 
-print(troco)
